0ffbf717df0491d1e7a73d1633cd2a8bda970a4b
VisionaryPotato <52299749+VisionaryPotato@users.noreply.github.com>
2020-08-28 14:42:12 -0600

    Add files via upload

---
+++ EclipseProjects/lab1/src/Employee.java
@@ -0,0 +1,53 @@
+public class Employee extends Person {
+    // An Employee IS A Person. It contains all of the data from Person, but a
+    // little bit more.
+    private long salary;
+    private String employer;
+    /* YOUR CODE HERE 
+     * It would make sense to record the company that this person
+     * works for as well. Under this comment, declare a new String field called
+     * "employer", and update the constructor below so that we can set it when
+     * we construct a new Employee object.
+     *
+     * Make your new employer parameter the LAST parameter in the constructor.
+     */
+
+    public Employee(String name, int age, long salary, String employer /* fourth param here */) {
+        super(name, age);
+        this.salary = salary;
+        this.employer = employer;
+        /* ... more code here! */
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+    
+    // Getters and setters for the new "employer" field.
+    public String getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(String employer) {
+        this.employer = employer;
+    }
+    
+    public String toString() {
+    	 return String.format("%s is %d years old. They make $%d a year at %s.", super.getName(), super.getAge(), getSalary(), getEmployer());
+    }
+    
+    /* YOUR CODE HERE
+     * Create a new toString that prints in the following format:
+     * "<name> is <age> years old. They make $<salary> a year at <employer>"
+     * Try to use super.toString().
+     */
+    
+    public static void main(String[] args) {
+        Employee e = new Employee("Samir", 28, 120000, "Initech");
+        System.out.println(e.toString());
+    }
+}
